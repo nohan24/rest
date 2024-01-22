4e00825a5186752c3f96e881ef987102c7cb1c95
nohan24 <nohanrazafinjato@gmail.com>
2024-01-22 21:10:12 +0300

    f

--- src/main/java/itu/repository/VenteRepo.java
+++ src/main/java/itu/repository/VenteRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface VenteRepo extends JpaRepository<Vente, Integer> {
-    @Query("select sum(v.prix) from Voiture v")
+    @Query("select sum(v.commission) from Vente v")
     double sum();
 }
